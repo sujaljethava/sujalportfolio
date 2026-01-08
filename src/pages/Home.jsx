3e41c9448364da2b141c5939a8e97ec23cb75a57
Sujal Jethava <sujaljethava13@gmail.com>
2026-01-08 21:45:35 +0530

    Chnages Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
         className="text-center"
       >
         <h1 className="text-5xl md:text-6xl font-extrabold drop-shadow-sm">
-          Hi, I'm <span className="text-blue-400">Kishan</span>
+          Hi, I'm <span className="text-blue-400">Sujal</span>
         </h1>
 
         <p className="mt-4 text-lg md:text-xl text-gray-300 max-w-2xl mx-auto">
