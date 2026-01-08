0d792d94a00c8adb1342dc554bbc914048ca2107
Sujal Jethava <sujaljethava13@gmail.com>
2026-01-08 10:11:24 +0530

    Changes Done

--- src/components/ChatFloatingButton.jsx
+++ src/components/ChatFloatingButton.jsx
@@ -3,23 +3,35 @@ import { FaComments } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function ChatFloatingButton() {
-  return (
-    <motion.div
-      initial={{ opacity: 0, scale: 0 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 0.5 }}
-      className="fixed bottom-6 right-6 z-50"
-    >
-      <motion.div
-        whileHover={{ scale: 1.15, rotate: 5 }}
-        whileTap={{ scale: 0.9 }}
-        className="bg-blue-600 hover:bg-blue-700 text-white p-4 rounded-full shadow-2xl cursor-pointer border border-white/10 backdrop-blur-lg"
-      >
-        {/* Change LINK as per your preference */}
-        <Link to="/contact">
+    return (
+        <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="fixed bottom-6 right-6 z-50"
+        >
+            <motion.div
+                whileHover={{ scale: 1.15, rotate: 5 }}
+                whileTap={{ scale: 0.9 }}
+                className="bg-blue-600 hover:bg-blue-700 text-white p-4 rounded-full shadow-2xl cursor-pointer border border-white/10 backdrop-blur-lg"
+            >
+                {/* Change LINK as per your preference */}
+                {/* <Link to="/contact">
           <FaComments className="text-3xl" />
-        </Link>
-      </motion.div>
-    </motion.div>
-  );
+        </Link> */}
+                {/* <a
+                    href="https://wa.me/919601389117"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <FaComments className="text-3xl" />
+                </a> */}
+                <a href="mailto:sujaljethava13@gmail.com">
+                    <FaComments className="text-3xl" />
+                </a>
+
+
+            </motion.div>
+        </motion.div>
+    );
 }
